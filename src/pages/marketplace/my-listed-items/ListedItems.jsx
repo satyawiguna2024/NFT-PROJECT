fd5bc92464694c6bb0c31dc222929b549c071747
I Made Satya Wiguna <satwig600@gmail.com>
2026-01-06 00:44:36 +0800

    Build listeditems

--- src/pages/marketplace/my-listed-items/ListedItems.jsx
+++ src/pages/marketplace/my-listed-items/ListedItems.jsx
@@ -1,14 +1,18 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { useAccount, usePublicClient } from "wagmi";
-import { CONTRACT_ADDRESS_MARKETPLACE, ABI_MARKETPLACE } from "../../../contract/Marketplace";
+import {
+  CONTRACT_ADDRESS_MARKETPLACE,
+  ABI_MARKETPLACE,
+} from "../../../contract/Marketplace";
 import { CONTRACT_ADDRESS_NFT, ABI_NFT } from "../../../contract/NFT";
+import { formatEther } from "viem";
 
 export default function ListedItems() {
   const [listItems, setListItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const publicClient = usePublicClient();
-  const {address} = useAccount();
+  const { address } = useAccount();
 
   // Convert IPFS -> HTTP Gateway
   const resolveIPFS = (uri) => {
@@ -18,9 +22,9 @@ export default function ListedItems() {
       : uri;
   };
 
-  const loadMyListedItems = async() => {
-    if(!address) {
-      setIsLoading(false)
+  const loadMyListedItems = async () => {
+    if (!address) {
+      setIsLoading(false);
       return;
     }
 
@@ -35,7 +39,7 @@ export default function ListedItems() {
 
       const myListedItems = [];
 
-      for(let i = 1; i <= Number(itemCount); i++) {
+      for (let i = 1; i <= Number(itemCount); i++) {
         const item = await publicClient.readContract({
           address: CONTRACT_ADDRESS_MARKETPLACE,
           abi: ABI_MARKETPLACE,
@@ -44,85 +48,127 @@ export default function ListedItems() {
         });
 
         // Mengecek apakah seller user address saat ini
-        if(item[4].toLowerCase() !== address.toLowerCase()) continue;
+        if (item[4].toLowerCase() !== address.toLowerCase()) continue;
 
         const tokenId = item[2];
 
         const uri = await publicClient.readContract({
-            address: CONTRACT_ADDRESS_NFT,
-            abi: ABI_NFT,
-            functionName: "tokenURI",
-            args: [tokenId],
-          });
+          address: CONTRACT_ADDRESS_NFT,
+          abi: ABI_NFT,
+          functionName: "tokenURI",
+          args: [tokenId],
+        });
 
         const metadataURL = resolveIPFS(uri);
 
-          const response = await fetch(metadataURL);
-          const metadata = await response.json();
+        const response = await fetch(metadataURL);
+        const metadata = await response.json();
 
-          const totalPrice = await publicClient.readContract({
-            address: CONTRACT_ADDRESS_MARKETPLACE,
-            abi: ABI_MARKETPLACE,
-            functionName: "getTotalPrice",
-            args: [i]
-          });
+        const totalPrice = await publicClient.readContract({
+          address: CONTRACT_ADDRESS_MARKETPLACE,
+          abi: ABI_MARKETPLACE,
+          functionName: "getTotalPrice",
+          args: [i],
+        });
 
-          myListedItems.push({
-            id: Number(item[0]),
-            tokenId: Number(tokenId),
-            price: item[3],
-            totalPrice,
-            seller: item[4],
-            sold: item[5],
-            name: metadata.name,
-            description: metadata.description,
-            image: resolveIPFS(metadata.image),
-          });
+        myListedItems.push({
+          id: Number(item[0]),
+          tokenId: Number(tokenId),
+          price: item[3],
+          totalPrice,
+          seller: item[4],
+          sold: item[5],
+          name: metadata.name,
+          description: metadata.description,
+          image: resolveIPFS(metadata.image),
+        });
 
-          setListItems(myListedItems);
+        setListItems(myListedItems);
       }
     } catch (err) {
       console.error("Error in loadMyListedItems: ", err.message);
     } finally {
       setIsLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
     loadMyListedItems();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  console.log("My NFT: ", listItems);
-
-  if (isLoading) return <span className="animate-pulse">Loading...</span>;
-
+  if (isLoading) return <span className="animate-pulse text-white">Loading...</span>;
 
   return (
     <>
-      <div className="w-full max-w-6xl mx-auto">
-        <div className="grid grid-cols-4 gap-3 mt-8">
-          {/* cards */}
-          {[1].map((_, i) => (
-            <div key={i} className="border border-gray-400 rounded-md">
+      <div className="container-costume p-3 mt-5">
+        <div className="flex justify-between items-center">
+          <h1 className="font-poppins font-semibold text-lg sm:text-xl lg:text-2xl text-gray-200 tracking-wide">
+            {listItems.length} Items
+          </h1>
+
+          <select className="border border-gray-200 text-gray-200 p-1 rounded-md">
+            <option value="">High to Low</option>
+            <option value="">Low to High</option>
+          </select>
+        </div>
+
+        {/* card */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mt-10">
+          {listItems.map((itm, i) => (
               <div
-                className="relative size-full h-80 bg-cover object-cover bg-center flex flex-col justify-end items-center rounded-b-none"
-                style={{
-                  backgroundImage:
-                    "url('https://images.unsplash.com/flagged/photo-1558706379-e9698f05d675?q=80&w=1646&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
-                }}
+                key={i}
+                className="bg-gray-600 p-3 rounded-xl max-w-70 mx-auto"
               >
-                <div className="w-full bg-linear-to-t from-black/80 via-black/40 to-transparent text-center">
-                  {/* title */}
-                  <h3 className="text-xl text-gray-200 font-semibold drop-shadow-lg">Dove</h3>
-                  {/* description */}
-                  <p className="text-lg text-gray-200 font-medium drop-shadow-lg pb-3">Majestic and Blue</p>
+                <div className="flex items-center gap-x-2">
+                  <div
+                    style={{
+                      backgroundImage: `url('https://api.dicebear.com/7.x/identicon/svg?seed=${itm.seller}')`,
+                    }}
+                    className="size-12 rounded-full p-5 bg-cover bg-center object-cover border border-gray-300 bg-white"
+                  />
+                  <div>
+                    <p className="font-poppins text-gray-200/70 font-medium">
+                      Creator
+                    </p>
+                    <h4 className="font-poppins text-gray-200 font-medium">
+                      {itm.seller.slice(0, 15)}...{itm.seller.slice(-4)}
+                    </h4>
+                  </div>
+                </div>
+
+                {/* image nft */}
+                <div className="relative">
+                  <img
+                    src={itm.image}
+                    alt="NFTs"
+                    className="w-full h-64 bg-cover bg-center p-2 object-cover rounded-3xl"
+                  />
+
+                  <h5 className={`absolute bottom-4 left-20 z-10 px-3 rounded-full font-poppins bg-black/60 backdrop-blur-md border ${itm.sold ? "text-red-400  border-red-400/40 shadow-[0_0_15px_rgba(239,68,68,0.6)]" : "text-green-400  border-green-400/40 shadow-[0_0_15px_rgba(34,197,94,0.6)]"} `}>
+                    {itm.sold ? "Sold Out" : "Available"}
+                  </h5>
+                </div>
+
+                {/* title */}
+                <div className="mx-2">
+                  <h3 className="font-poppins text-gray-200 font-semibold text-lg">
+                    "{itm.name}"
+                  </h3>
+
+                  <div className="flex items-end justify-between mt-3">
+                    <div className="leading-tight">
+                      <p className="text-xs text-gray-400 font-medium">Price</p>
+                      <p className="text-gray-200 font-semibold text-lg">
+                        {formatEther(itm.totalPrice)} ETH
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
           ))}
         </div>
       </div>
     </>
-  )
+  );
 }
